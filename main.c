406f87bc54869770b3ffc97d07430aab048e5d30
knight <1336302623@qq.com>
2023-02-28 10:13:01 +0800

    add: output error by marco

--- main.c
+++ main.c
@@ -5,13 +5,17 @@
 #include <sys/inotify.h>
 #include <sys/epoll.h>
 
+#define _throw_return(info, return_val...) \
+    perror(info); \
+    return return_val;
+
+
 void change_brightness() {
     FILE *fp = fopen("/sys/class/backlight/nvidia_0/brightness", "r");
     if (fp == NULL) {
-        perror("fopen nvidia");
-        return;
+        _throw_return("fopen nvidia");
     }
-    
+
     int brightness_nvidia, brightness_amd;
     if (fscanf(fp, "%d", &brightness_nvidia) == EOF) {
         return;
@@ -21,8 +25,7 @@ void change_brightness() {
 
     fp = fopen("/sys/class/backlight/amdgpu_bl0/brightness", "w");
     if (fp == NULL) {
-        perror("fopen amd");
-        return;
+        _throw_return("fopen amd");
     }
     fprintf(fp, "%d", brightness_amd);
     fflush(fp);
@@ -35,13 +38,11 @@ int main() {
     
     int fd = inotify_init1(IN_NONBLOCK);
     if (fd == -1) {
-        perror("inotify_init1");
-        exit(EXIT_FAILURE);
+        _throw_return("inotify_init1", -1);
     }
 
     if (inotify_add_watch(fd, file_path, IN_CLOSE_WRITE) == -1) {
-        perror("add_watch");
-        exit(EXIT_FAILURE);
+        _throw_return("add_watch", -1);
     }
 
     char buf[4096] 
@@ -52,27 +53,23 @@ int main() {
     struct epoll_event ev, events[1];
     int epfd = epoll_create1(0);
     if (epfd == -1) {
-        perror("epoll_create1");
-        exit(EXIT_FAILURE);
+        _throw_return("epoll_create1", -1);
     }
 
     ev.events = EPOLLIN;
     ev.data.fd = fd;
     if (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev) == -1) {
-        perror("epoll_ctl");
-        exit(EXIT_FAILURE);
+        _throw_return("epoll_ctl", -1);
     }
 
     for (;;) {
         if (epoll_wait(epfd, events, 1, -1) == -1) {
-            perror("epoll_wait");
-            exit(EXIT_FAILURE);
+            _throw_return("epoll_wait", -1);
         }
         
         len = read(fd, buf, sizeof(buf));
         if (len == -1) {
-            perror("read inofd");
-            exit(EXIT_FAILURE);
+            _throw_return("read inofd", -1);
         }
         
         for (char *ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len) {
