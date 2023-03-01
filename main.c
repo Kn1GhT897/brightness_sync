d0e378a6f9808c4f31adc98ce10db05330616959
knight <1336302623@qq.com>
2023-03-01 15:34:47 +0800

    refactor

--- main.c
+++ main.c
@@ -1,17 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <sys/inotify.h>
 #include <sys/epoll.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
+
+#define nv_file_path "/sys/class/backlight/nvidia_0/brightness"
+#define amd_file_path "/sys/class/backlight/amdgpu_bl0/brightness"
 #define _throw_return(info, return_val...) \
     perror(info); \
     return return_val;
+#define _throw_exit(info) \
+    perror(info); \
+    exit(EXIT_FAILURE);
+
+
+int init_inotify_fd(int*, int*, u_int32_t);
+int init_epoll_fd(int*, int);
+int get_inode(int);
+bool check_inotify_event(char*, ssize_t, const struct inotify_event*, u_int32_t);
+void change_brightness();
+
+
+int main() {
+    char buf[4096] 
+        __attribute__ ((aligned(__alignof__(struct inotify_event))));
+    const struct inotify_event *inotify_events;
+    ssize_t len;
+
+    struct epoll_event epoll_events[1];
+
+    int fd, wd, inode;
+    u_int32_t mask = IN_CLOSE_WRITE;
+    if (init_inotify_fd(&fd, &wd, mask) == -1) {
+        return -1;
+    }
+    inode = get_inode(fd);
+
+    int epoll_fd;
+    if (init_epoll_fd(&epoll_fd, fd) == -1) {
+        return -1;
+    }
+
+    int nr;
+    for (;;) {
+        do {
+            nr = epoll_wait(epoll_fd, epoll_events, 1, -1);
+        } while (nr < 0 && errno == EINTR);
+
+        len = read(fd, buf, sizeof(buf));
+        if (len == -1) {
+            continue;
+        }
+        
+        if (check_inotify_event(buf, len, inotify_events, mask)) {
+            change_brightness();
+        }
+    }
+    return 0;
+}
+
+int init_inotify_fd(int *fd, int *wd, u_int32_t mask) {
+    /*
+        *fd: the file descriptor to be init
+        *wd: the watch descriptor to be init
+        mask: inotify mask
+    */
+    *fd = inotify_init1(IN_NONBLOCK);
+    if (*fd == -1) {
+        _throw_return("inotify_init1", -1);
+    }
+    *wd = inotify_add_watch(*fd, nv_file_path, mask);
+    if (*wd == -1) {
+        _throw_return("inotify_add_watch", -1);
+    }
+    return 0;
+}
+
+int init_epoll_fd(int *epoll_fd, int fd) {
+    /*
+        *epoll_fd: as its name
+        fd: as its name 
+    */
+    struct epoll_event ev;
+    *epoll_fd = epoll_create1(0);
+    if (*epoll_fd == -1) {
+        _throw_return("epoll_create1", -1);
+    }
+
+    ev.events = EPOLLIN;
+    ev.data.fd = fd;
+    if (epoll_ctl(*epoll_fd, EPOLL_CTL_ADD, fd, &ev) == -1) {
+        _throw_return("epoll_ctl", -1);
+    }
+    return 0;
+}
+
+int get_inode(int fd) {
+    struct stat file_stat;
+    if (fstat(fd, &file_stat) < 0) {
+        _throw_exit("fstat");
+    }
+    return file_stat.st_ino;
+}
+
+bool check_inotify_event(char* buf, ssize_t len, const struct inotify_event* inotify_events, u_int32_t mask) {
+    for (char *ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + inotify_events->len) {
+        inotify_events = (const struct inotify_event*) ptr;
+        if (inotify_events->mask & mask) { 
+            return true;
+        }
+    }
+    return false;
+}
 
 
 void change_brightness() {
-    FILE *fp = fopen("/sys/class/backlight/nvidia_0/brightness", "r");
+    FILE *fp = fopen(nv_file_path, "r");
     if (fp == NULL) {
         _throw_return("fopen nvidia");
     }
@@ -23,63 +133,11 @@ void change_brightness() {
     brightness_amd = 255 * brightness_nvidia / 100;
     fclose(fp);
 
-    fp = fopen("/sys/class/backlight/amdgpu_bl0/brightness", "w");
+    fp = fopen(amd_file_path, "w");
     if (fp == NULL) {
         _throw_return("fopen amd");
     }
     fprintf(fp, "%d", brightness_amd);
     fflush(fp);
     fclose(fp);
-}
-
-
-int main() {
-    char *file_path = "/sys/class/backlight/nvidia_0/brightness";
-    
-    int fd = inotify_init1(IN_NONBLOCK);
-    if (fd == -1) {
-        _throw_return("inotify_init1", -1);
-    }
-
-    if (inotify_add_watch(fd, file_path, IN_CLOSE_WRITE) == -1) {
-        _throw_return("add_watch", -1);
-    }
-
-    char buf[4096] 
-        __attribute__ ((aligned(__alignof__(struct inotify_event))));
-    const struct inotify_event *event;
-    ssize_t len;
-    
-    struct epoll_event ev, events[1];
-    int epfd = epoll_create1(0);
-    if (epfd == -1) {
-        _throw_return("epoll_create1", -1);
-    }
-
-    ev.events = EPOLLIN;
-    ev.data.fd = fd;
-    if (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev) == -1) {
-        _throw_return("epoll_ctl", -1);
-    }
-
-    for (;;) {
-        int nr;
-        do {
-            nr = epoll_wait(epfd, events, 1, -1);
-        } while (nr < 0 && errno == EINTR);
-        
-        len = read(fd, buf, sizeof(buf));
-        if (len == -1) {
-            _throw_return("read inofd", -1);
-        }
-        
-        for (char *ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len) {
-            event = (const struct inotify_event*) ptr;
-            if (event->mask & IN_CLOSE_WRITE) { 
-                change_brightness();
-            }
-        }
-    }
-
-    return 0;
 }
--- makefile
+++ makefile
@@ -1,11 +1,14 @@
-run: build
-	./target/brightness_sync
-	
 build: main.c
-	gcc -O3 main.c -o brightness_sync
-	sudo chown root brightness_sync
-	sudo chmod 4777 brightness_sync 
-	mv ./brightness_sync ./target/brightness_sync  
+	gcc -O3 main.c -o ./target/debug/brightness_sync
+	sudo chown root ./target/debug/brightness_sync
+	sudo chmod 4777 ./target/debug/brightness_sync
+
+run: build
+	./target/debug/brightness_sync
+
+release: build
+	cp ./target/debug/brightness_sync ./target/release/brightness_sync 
 
 clear: build
-	rm ./target/brightness_sync
+	rm ./target/debug/brightness_sync
+	rm ./target/release/brightness_sync
